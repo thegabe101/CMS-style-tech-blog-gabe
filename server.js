d745dbd457480d3017e7cde2132ad640b2f3749f
Rayncor <sowa332@gmail.com>
2022-08-02 19:34:13 -0700

    added sequelize store, style css base, trying to determine which handlebars to build first

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const Router = require("./routes");
 const session = require("express-session");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 const handlebars = require("express-handlebars");
 const sequelize = require("./config/connection.js");
 const express = require("express");
@@ -25,18 +26,29 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
+//TODO: have CSS in wrong folder right now- needs to go to static public folder
 
 //finally, instruct our app to use our router
 app.use(Router)
 
 //next we need to create a session instance. im not sure how to do this yet so will wait till after class. 
 //TODO: create session
+//learned how to make a session in class today. first we need to require sequelizestore, which we will do up above. 
+//next, we declare a session with several parameters
+const yourSession = {
+    secret: "nosecret",
+    saveUnitialized: false,
+    reserve: false,
+    store: new sequelizeStore ({
+        db: sequelize
+    })
+};
 
 
 //finally we can set up our sequelize sync and port listen
 sequelize.sync({force: false}).then(() => {
-    app.listen(process.env.PORT || 3001, () => {
-        console.log("App is listening at + ${PORT}");
+    app.listen(process.env.PORT || 8080, () => {
+        console.log("The server is now online. Please navigate to localhost 8080 or to your environmental port.");
     });;
 });
 
